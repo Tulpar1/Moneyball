b25062e41214387d7f74d1364eb3d894d40e771f
Tulpar1 <atakan.culban@gmail.com>
2025-11-24 00:00:12 +0300

    Added Routing among Tables.
    Added routing skeleton and implemeted it to better understanding of what we are doing for all members and a begging for indepentend occurnes of tables .

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from flask import Flask, render_template
 
-# Flask uygulamasını başlat
 app = Flask(__name__)
 
-# Ana sayfa rotası (URL: /)
+
+DB_TABLES = [
+    "Appearances",
+    "club_games",
+    "clubs",
+    "competitions",
+    "players",
+    "playervaluations"
+]
+
+
 @app.route('/')
 def index():
-    """
-    Ana sayfayı render eder.
-    Flask, 'render_template' kullandığında varsayılan olarak 'templates' klasörüne bakar.
-    """
-    return render_template('base.html')
+    return render_template('index.html', tables=DB_TABLES)
 
-# Bu blok, dosya doğrudan çalıştırıldığında sunucuyu başlatır
-# (Örn: python main.py)
+
+@app.route('/table/<table_name>')
+def show_table(table_name):
+
+    if table_name not in DB_TABLES:
+        return "Table not found", 404
+
+    return render_template('table.html', table_name=table_name)
+
+
+@app.route('/table/<table_name>/add')
+def add_record(table_name):
+    if table_name not in DB_TABLES:
+        return "Table not found", 404
+    return f"<h1>Add to {table_name} table</h1>"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
